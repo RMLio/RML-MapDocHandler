37e3f7abf3459de42105387eb6c8952a2dd8a64b
andimou <anastasia.dimou@ugent.be>
2016-03-17 14:08:52 +0100

    validation extractor

---
+++ src/main/java/be/ugent/mmlab/rml/mapdochandler/extraction/concrete/CombustExtractor.java
@@ -0,0 +1,74 @@
+
+package be.ugent.mmlab.rml.mapdochandler.extraction.concrete;
+
+import be.ugent.mmlab.rml.vocabularies.CoVocabulary;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.ValueFactory;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * RML Processor
+ *
+ * @author andimou
+ */
+public class CombustExtractor {
+    // Log
+    static final Logger log = LoggerFactory.getLogger(
+            CombustExtractor.class.getSimpleName());
+    
+    public boolean exrtactValidate(Resource term, Repository repository) {
+        try {
+            RepositoryConnection connection = repository.getConnection();
+            ValueFactory vf = connection.getValueFactory();
+
+            URI uri = vf.createURI(CoVocabulary.CO_NAMESPACE
+                    + CoVocabulary.COTerm.REQUIRES);
+            URI obj = vf.createURI(CoVocabulary.CO_NAMESPACE
+                    + CoVocabulary.COTerm.VALIDATION_CLASS);
+
+            RepositoryResult<Statement> validation_statements =
+                    connection.getStatements(term, uri, obj, true);
+
+            if (validation_statements.hasNext()) {
+                log.debug("Term Map to be validated");
+                return true;
+            }
+
+        } catch (RepositoryException ex) {
+            log.error("Repository Exception " + ex);
+        }
+        return false;
+    }
+    
+    public boolean exrtactComplete(Resource term, Repository repository){
+        try {
+            RepositoryConnection connection = repository.getConnection();
+            ValueFactory vf = connection.getValueFactory();
+
+            URI uri = vf.createURI(CoVocabulary.CO_NAMESPACE
+                    + CoVocabulary.COTerm.REQUIRES);
+            URI obj = vf.createURI(CoVocabulary.CO_NAMESPACE
+                    + CoVocabulary.COTerm.COMPLETION_CLASS);
+            
+            RepositoryResult<Statement> statements = 
+                    connection.getStatements(term, uri, obj, true);
+
+                if(statements.hasNext()){
+                    log.debug("Term Map to be completed");
+                    return true;
+                }
+            
+        } catch (RepositoryException ex) {
+            log.error("Repository Exception " + ex);
+        }
+        return false;
+    }
+
+}
