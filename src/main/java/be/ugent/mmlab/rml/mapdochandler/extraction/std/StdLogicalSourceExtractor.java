af564bd2d0d68d9e17509c1986ca8a57705af321
Ben De Meester <ben.demeester@ugent.be>
2018-04-25 11:14:17 +0200

    removed dead code

--- src/main/java/be/ugent/mmlab/rml/mapdochandler/extraction/std/StdLogicalSourceExtractor.java
+++ src/main/java/be/ugent/mmlab/rml/mapdochandler/extraction/std/StdLogicalSourceExtractor.java
@@ -38,42 +38,42 @@ public class StdLogicalSourceExtractor {
             LoggerFactory.getLogger(
             StdLogicalSourceExtractor.class.getSimpleName());
 
-    /**
-     *
-     * @return
-     */
-    public Map<Resource, Source> extractInputResources(Repository repository) {
-        Map<Resource, Source> inputResources = new HashMap<Resource, Source>();
+//    /**
+//     *
+//     * @return
+//     */
+//    public Map<Resource, Source> extractInputResources(Repository repository) {
+//        Map<Resource, Source> inputResources = new HashMap<Resource, Source>();
+//
+//        RepositoryResult<Statement> statements = getInputResources(repository);
+//
+//        inputResources = putInputResources(repository, statements, inputResources);
+//
+//        return inputResources;
+//    }
 
-        RepositoryResult<Statement> statements = getInputResources(repository);
-
-        inputResources = putInputResources(repository, statements, inputResources);
-
-        return inputResources;
-    }
-
-    /**
-     *
-     * @return
-     */
-    protected RepositoryResult<Statement> getInputResources(Repository repository) {
-        RepositoryResult<Statement> inputStatements = null;
-        try {
-
-            RepositoryConnection connection = repository.getConnection();
-            ValueFactory vf = connection.getValueFactory();
-
-            IRI p = vf.createIRI(RMLVocabulary.RML_NAMESPACE
-                    + RMLVocabulary.RMLTerm.SOURCE);
-            inputStatements = connection.getStatements(null, p, null, true);
-
-            connection.close();
-
-        } catch (RepositoryException ex) {
-            log.error("RepositoryException " + ex);
-        }
-        return inputStatements;
-    }
+//    /**
+//     *
+//     * @return
+//     */
+//    protected RepositoryResult<Statement> getInputResources(Repository repository) {
+//        RepositoryResult<Statement> inputStatements = null;
+//        try {
+//
+//            RepositoryConnection connection = repository.getConnection();
+//            ValueFactory vf = connection.getValueFactory();
+//
+//            IRI p = vf.createIRI(RMLVocabulary.RML_NAMESPACE
+//                    + RMLVocabulary.RMLTerm.SOURCE);
+//            inputStatements = connection.getStatements(null, p, null, true);
+//
+//            connection.close();
+//
+//        } catch (RepositoryException ex) {
+//            log.error("RepositoryException " + ex);
+//        }
+//        return inputStatements;
+//    }
 
     /**
      *
@@ -81,94 +81,94 @@ public class StdLogicalSourceExtractor {
      * @param inputResources
      * @return
      */
-    protected Map<Resource, Source> putInputResources(Repository repository,
-            RepositoryResult<Statement> statements, Map<Resource, Source> inputResources) {
-        try {
-            RepositoryConnection connection = repository.getConnection();
-            ValueFactory vf = connection.getValueFactory();
-
-            IRI p = vf.createIRI(RMLVocabulary.RML_NAMESPACE
-                    + RMLVocabulary.RMLTerm.LOGICAL_SOURCE);
-
-            while (statements.hasNext()) {
-                RepositoryResult<Statement> triplesMapsStatements =
-                        connection.getStatements(null, p, statements.next().getSubject(), true);
-
-                while (triplesMapsStatements.hasNext()) {
-                    try {
-                        inputResources.put(
-                                //triplesMap resource
-                                triplesMapsStatements.next().getSubject(),
-                                //input source
-                                new StdSource(triplesMapsStatements.next().getObject().stringValue()));
-                    } catch (Exception ex) {
-                        log.error(StdLogicalSourceExtractor.class.getName() + ex);
-                    }
-                }
-            }
-        } catch (RepositoryException ex) {
-            log.error("RepositoryException " + ex);
-        }
-        return inputResources;
-    }
+//    protected Map<Resource, Source> putInputResources(Repository repository,
+//            RepositoryResult<Statement> statements, Map<Resource, Source> inputResources) {
+//        try {
+//            RepositoryConnection connection = repository.getConnection();
+//            ValueFactory vf = connection.getValueFactory();
+//
+//            IRI p = vf.createIRI(RMLVocabulary.RML_NAMESPACE
+//                    + RMLVocabulary.RMLTerm.LOGICAL_SOURCE);
+//
+//            while (statements.hasNext()) {
+//                RepositoryResult<Statement> triplesMapsStatements =
+//                        connection.getStatements(null, p, statements.next().getSubject(), true);
+//
+//                while (triplesMapsStatements.hasNext()) {
+//                    try {
+//                        inputResources.put(
+//                                //triplesMap resource
+//                                triplesMapsStatements.next().getSubject(),
+//                                //input source
+//                                new StdSource(triplesMapsStatements.next().getObject().stringValue()));
+//                    } catch (Exception ex) {
+//                        log.error(StdLogicalSourceExtractor.class.getName() + ex);
+//                    }
+//                }
+//            }
+//        } catch (RepositoryException ex) {
+//            log.error("RepositoryException " + ex);
+//        }
+//        return inputResources;
+//    }
 
     /**
      *
      * @param inputResource
      * @param inputResources
      */
-    public void extractInputSource(
-            Repository repository, Resource inputResource,
-            Map<Resource, Source> inputResources) {
-        log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + ": "
-                + "Extract Input Resource : "
-                + inputResource.stringValue());
-
-        Source result = inputResources.get(inputResource);
-
-        // Extract TriplesMap properties
-        Set<TriplesMap> triplesMaps =
-                extractTriplesMaps(repository, inputResource);
-
-        // Add triples maps
-        for (TriplesMap triplesMap : triplesMaps) {
-            //result.setTriplesMap(triplesMap);
-        }
-
-        log.info(Thread.currentThread().getStackTrace()[1].getMethodName() + ": "
-                + "Extract of Input source : "
-                + inputResource.stringValue() + " done.");
-    }
+//    public void extractInputSource(
+//            Repository repository, Resource inputResource,
+//            Map<Resource, Source> inputResources) {
+//        log.debug(Thread.currentThread().getStackTrace()[1].getMethodName() + ": "
+//                + "Extract Input Resource : "
+//                + inputResource.stringValue());
+//
+//        Source result = inputResources.get(inputResource);
+//
+//        // Extract TriplesMap properties
+//        Set<TriplesMap> triplesMaps =
+//                extractTriplesMaps(repository, inputResource);
+//
+//        // Add triples maps
+//        for (TriplesMap triplesMap : triplesMaps) {
+//            //result.setTriplesMap(triplesMap);
+//        }
+//
+//        log.info(Thread.currentThread().getStackTrace()[1].getMethodName() + ": "
+//                + "Extract of Input source : "
+//                + inputResource.stringValue() + " done.");
+//    }
 
     /**
      *
      * @param inputResource
      * @return
      */
-    protected Set<TriplesMap> extractTriplesMaps(
-            Repository repository, Resource inputResource) {
-        Set<TriplesMap> triplesMaps = new HashSet<TriplesMap>();
-        try {
-            TriplesMap triplesMap;
-
-            RepositoryConnection connection = repository.getConnection();
-
-            IRI p = getTermURI(repository, RMLVocabulary.RMLTerm.LOGICAL_SOURCE);
-            RepositoryResult<Statement> triplesMapStatements =
-                    connection.getStatements(null, p, inputResource, true);
-
-            while (triplesMapStatements.hasNext()) {
-                triplesMap = new StdTriplesMap(
-                        null, null, null, triplesMapStatements.next().getSubject().stringValue());
-                triplesMaps.add(triplesMap);
-            }
-            connection.close();
-
-        } catch (RepositoryException ex) {
-            log.error("RepositoryException " + ex);
-        }
-        return triplesMaps;
-    }
+//    protected Set<TriplesMap> extractTriplesMaps(
+//            Repository repository, Resource inputResource) {
+//        Set<TriplesMap> triplesMaps = new HashSet<TriplesMap>();
+//        try {
+//            TriplesMap triplesMap;
+//
+//            RepositoryConnection connection = repository.getConnection();
+//
+//            IRI p = getTermURI(repository, RMLVocabulary.RMLTerm.LOGICAL_SOURCE);
+//            RepositoryResult<Statement> triplesMapStatements =
+//                    connection.getStatements(null, p, inputResource, true);
+//
+//            while (triplesMapStatements.hasNext()) {
+//                triplesMap = new StdTriplesMap(
+//                        null, null, null, triplesMapStatements.next().getSubject().stringValue());
+//                triplesMaps.add(triplesMap);
+//            }
+//            connection.close();
+//
+//        } catch (RepositoryException ex) {
+//            log.error("RepositoryException " + ex);
+//        }
+//        return triplesMaps;
+//    }
 
     /**
      *
@@ -177,48 +177,48 @@ public class StdLogicalSourceExtractor {
      * @param triplesMap
      * @return
      */
-    protected RepositoryResult<Statement> getStatements(
-            Repository repository, Enum term, Resource resource, TriplesMap triplesMap) {
-        RepositoryResult<Statement> statements = null;
-        try {
-            IRI p = getTermURI(repository, term);
-            RepositoryConnection connection = repository.getConnection();
-            statements = connection.getStatements(resource, p, null, true);
-
-            connection.close();
-        } catch (RepositoryException ex) {
-            log.error("RepositoryException " + ex);
-        }
-        return statements;
-    }
+//    protected RepositoryResult<Statement> getStatements(
+//            Repository repository, Enum term, Resource resource, TriplesMap triplesMap) {
+//        RepositoryResult<Statement> statements = null;
+//        try {
+//            IRI p = getTermURI(repository, term);
+//            RepositoryConnection connection = repository.getConnection();
+//            statements = connection.getStatements(resource, p, null, true);
+//
+//            connection.close();
+//        } catch (RepositoryException ex) {
+//            log.error("RepositoryException " + ex);
+//        }
+//        return statements;
+//    }
 
     /**
      *
      * @param term
      * @return
      */
-    protected static IRI getTermURI(Repository repository, Enum term) {
-        String namespace = R2RMLVocabulary.R2RML_NAMESPACE;
-
-        if (term instanceof RMLVocabulary.RMLTerm) {
-            namespace = RMLVocabulary.RML_NAMESPACE;
-        } else if ((term instanceof R2RMLVocabulary.R2RMLTerm)) {
-            namespace = R2RMLVocabulary.R2RML_NAMESPACE;
-        } else {
-            log.error(Thread.currentThread().getStackTrace()[1].getMethodName() + ": "
-                    + term + " is not valid.");
-        }
-
-        RepositoryConnection connection;
-        IRI uri = null;
-        try {
-            connection = repository.getConnection();
-            ValueFactory vf = connection.getValueFactory();
-            uri = vf.createIRI(namespace + term);
-            connection.close();
-        } catch (RepositoryException ex) {
-            log.error("RepositoryException " + ex);
-        }
-        return uri;
-    }
+//    protected static IRI getTermURI(Repository repository, Enum term) {
+//        String namespace = R2RMLVocabulary.R2RML_NAMESPACE;
+//
+//        if (term instanceof RMLVocabulary.RMLTerm) {
+//            namespace = RMLVocabulary.RML_NAMESPACE;
+//        } else if ((term instanceof R2RMLVocabulary.R2RMLTerm)) {
+//            namespace = R2RMLVocabulary.R2RML_NAMESPACE;
+//        } else {
+//            log.error(Thread.currentThread().getStackTrace()[1].getMethodName() + ": "
+//                    + term + " is not valid.");
+//        }
+//
+//        RepositoryConnection connection;
+//        IRI uri = null;
+//        try {
+//            connection = repository.getConnection();
+//            ValueFactory vf = connection.getValueFactory();
+//            uri = vf.createIRI(namespace + term);
+//            connection.close();
+//        } catch (RepositoryException ex) {
+//            log.error("RepositoryException " + ex);
+//        }
+//        return uri;
+//    }
 }
